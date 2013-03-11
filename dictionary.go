02f78c0502f94f3670b1c913ec86b31bb11b1864
Sergey Gerasimov <mail@grsmv.com>
2013-03-11 23:09:40 +0200

    WIP

--- dictionary.go
+++ dictionary.go
@@ -19,40 +19,35 @@ type Bif struct {
 func main() {
     var rawDictionary = [][]string{
 
-        // packages
-        []string{"package",        "install",           "package-install",         "1"},
-        []string{"package",        "update",            "package-update",          "1"},
-        []string{"package",        "remove",            "package-remove",          "1"},
-        []string{"package",        "list",              "package-list",            "0"},
+        //       package-name             goName               lispName                   arity
+        // ------------------------------------------------------------------------------------
 
-        // apt-keys
-        []string{"aptKey",         "addFromUrl",        "add-key-from-url",        "1"},
-        []string{"aptKey",         "addFromKeyserver",  "add-key-from-keyserver",  "1"},
-        []string{"aptKey",         "addFromFile",       "add-key-from-file",       "1"},
+        []string {"package",              "install",           "package-install",         "1"},
+        []string {"package",              "update",            "package-update",          "1"},
+        []string {"package",              "remove",            "package-remove",          "1"},
+        []string {"package",              "list",              "package-list",            "0"},
 
-        // repositories
-        []string{"aptRepository",  "add",               "add-repository",          "1"},
+        []string {"packageRepositoryKey", "addFromUrl",        "add-key-from-url",        "1"},
+        []string {"packageRepositoryKey", "addFromKeyserver",  "add-key-from-keyserver",  "1"},
+        []string {"packageRepositoryKey", "addFromFile",       "add-key-from-file",       "1"},
 
-        // debconf
-        []string{"debconf",        "setSelections",     "debconf-set-selections",  "2"},
+        []string {"packageRepository",    "add",               "add-repository",          "1"},
 
-        // services
-        []string{"service",        "start",             "service-start",           "1"},
-        []string{"service",        "stop",              "service-stop",            "1"},
-        []string{"service",        "restart",           "service-restart",         "1"},
+        []string {"debconf",              "setSelections",     "debconf-set-selections",  "2"},
 
-        // services
-        []string{"update",         "start",             "update-start",            "1"},
-        []string{"update",         "stop",              "update-stop",             "1"},
-        []string{"update",         "restart",           "update-restart",          "1"},
+        []string {"service",              "start",             "service-start",           "1"},
+        []string {"service",              "stop",              "service-stop",            "1"},
+        []string {"service",              "restart",           "service-restart",         "1"},
 
-        // git
-        []string{"git",            "clone",             "git-clone",               "2"},
+        []string {"update",               "start",             "update-start",            "1"},
+        []string {"update",               "stop",              "update-stop",             "1"},
+        []string {"update",               "restart",           "update-restart",          "1"},
 
-        // common
-        []string{"common",         "eval",              "eval",                    "1"},
-        []string{"common",         "exec",              "exec",                    "1"},
-        []string{"common",         "exists",            "exists?",                 "1"}}
+        []string {"git",                  "clone",             "git-clone",               "2"},
+
+        []string {"common",               "eval",              "eval",                    "1"},
+        []string {"common",               "exec",              "exec",                    "1"},
+        []string {"common",               "exists",            "exists?",                 "1"}}
 
     bifs := Bifs { dictionary: []Bif {} }
 
@@ -60,10 +55,10 @@ func main() {
         arity, _ := strconv.Atoi(rawDictionary[index][3])
 
         bif := Bif { 
-            pkg: rawDictionary[index][0],
-            goName: rawDictionary[index][1],
-            lispName: rawDictionary[index][2],
-            arity: arity }
+            pkg:       rawDictionary[index][0],
+            goName:    rawDictionary[index][1],
+            lispName:  rawDictionary[index][2],
+            arity:     arity }
 
         bifs.dictionary = append(bifs.dictionary, bif)
     }
